6bf585b0b8c9bcab40ac460078f2ad59a9490ab6
Sebastian Pineda <63763483+sebascamilo12@users.noreply.github.com>
2022-07-12 22:25:14 -0500

    Create Application

---
+++ initial/src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        GreetingClient greetingClient = context.getBean(GreetingClient.class);
+        System.out.println(">>message = " + greetingClient.getMessage().block());
+    }
+}
